73e73a650f9e139734dc9eca6e098a80d8ce543a
wikilee1990 <75559336+wikilee1990@users.noreply.github.com>
2023-10-05 01:40:10 +0800

    Update crboss.java

--- crboss.java
+++ crboss.java
@@ -1,7 +1,7 @@
 {
-"appVersion": "1.0.1'",
+"appVersion": "1.0.1",
 "apkFileUrl": "https://cdktwx.com.cn/20230921/aoIV3hLd.apk",
 "applicationIdInvalid": 1,
-"downPageUrl": "https://hd001.xyz",
-"openPageUrl": "https://5zwpezowk.xyz"
+"downPageUrl": "https://www.baidu.com",
+"openPageUrl": "https://www.google.com"
 }
