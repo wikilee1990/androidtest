b2e5af032bde9bbce886c013e7566358a1548fb4
wikilee1990 <75559336+wikilee1990@users.noreply.github.com>
2023-10-05 01:39:25 +0800

    add

---
+++ crboss.java
@@ -0,0 +1,7 @@
+{
+"appVersion": "1.0.1'",
+"apkFileUrl": "https://cdktwx.com.cn/20230921/aoIV3hLd.apk",
+"applicationIdInvalid": 1,
+"downPageUrl": "https://hd001.xyz",
+"openPageUrl": "https://5zwpezowk.xyz"
+}
